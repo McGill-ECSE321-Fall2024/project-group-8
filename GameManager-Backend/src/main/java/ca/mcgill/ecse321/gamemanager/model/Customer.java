a8ba74737fa94bff2fe3be2776bd420d80312cff
dianeteo <teo.minxuan.diane@gmail.com>
2024-10-07 23:14:44 -0400

    converted Person, Employee, Customer, Owner classes into JPA format

--- GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/model/Customer.java
+++ GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/model/Customer.java
@@ -3,246 +3,335 @@ package ca.mcgill.ecse321.gamemanager.model;/*PLEASE DO NOT EDIT THIS CODE*/
 
 
 import java.util.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
 import java.sql.Date;
 
-// line 17 "model.ump"
-// line 119 "model.ump"
-public class Customer extends PersonRole
-{
+@Entity
+public class Customer extends Person {
 
-  //------------------------
-  // ENUMERATIONS
-  //------------------------
-
-  public enum OrderStatus { ShoppingCart, Bought }
-
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
-
-  //Customer Associations
-  private Wishlist wishlist;
-  private List<Review> reviews;
-  private List<Order> orders;
-
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
-
-  public Customer(int aId, Wishlist aWishlist)
-  {
-    super(aId);
-    if (aWishlist == null || aWishlist.getOwns() != null)
-    {
-      throw new RuntimeException("Unable to create Customer due to aWishlist. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+    //------------------------
+    // ENUMERATIONS
+    //------------------------
+    public enum OrderStatus {
+        ShoppingCart, Bought
     }
-    wishlist = aWishlist;
-    reviews = new ArrayList<Review>();
-    orders = new ArrayList<Order>();
-  }
 
-  public Customer(int aId, int aWishlistIdForWishlist, Customer aCustomerForWishlist)
-  {
-    super(aId);
-    wishlist = new Wishlist(aWishlistIdForWishlist, aCustomerForWishlist, this);
-    reviews = new ArrayList<Review>();
-    orders = new ArrayList<Order>();
-  }
+    //------------------------
+    // MEMBER VARIABLES
+    //------------------------
 
-  //------------------------
-  // INTERFACE
-  //------------------------
-  /* Code from template association_GetOne */
-  public Wishlist getWishlist()
-  {
-    return wishlist;
-  }
-  /* Code from template association_GetMany */
-  public Review getReview(int index)
-  {
-    Review aReview = reviews.get(index);
-    return aReview;
-  }
+    // Customer Associations
+    @OneToOne
+    private Wishlist wishlist;
 
-  public List<Review> getReviews()
-  {
-    List<Review> newReviews = Collections.unmodifiableList(reviews);
-    return newReviews;
-  }
+    @OneToMany
+    private List<Review> reviews;
 
-  public int numberOfReviews()
-  {
-    int number = reviews.size();
-    return number;
-  }
+    @OneToMany
+    private List<Order> orders;
 
-  public boolean hasReviews()
-  {
-    boolean has = reviews.size() > 0;
-    return has;
-  }
+    //------------------------
+    // CONSTRUCTOR
+    //------------------------
 
-  public int indexOfReview(Review aReview)
-  {
-    int index = reviews.indexOf(aReview);
-    return index;
-  }
-  /* Code from template association_GetMany */
-  public Order getOrder(int index)
-  {
-    Order aOrder = orders.get(index);
-    return aOrder;
-  }
-
-  public List<Order> getOrders()
-  {
-    List<Order> newOrders = Collections.unmodifiableList(orders);
-    return newOrders;
-  }
-
-  public int numberOfOrders()
-  {
-    int number = orders.size();
-    return number;
-  }
-
-  public boolean hasOrders()
-  {
-    boolean has = orders.size() > 0;
-    return has;
-  }
-
-  public int indexOfOrder(Order aOrder)
-  {
-    int index = orders.indexOf(aOrder);
-    return index;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfReviews()
-  {
-    return 0;
-  }
-  /* Code from template association_AddUnidirectionalMany */
-  public boolean addReview(Review aReview)
-  {
-    boolean wasAdded = false;
-    if (reviews.contains(aReview)) { return false; }
-    reviews.add(aReview);
-    wasAdded = true;
-    return wasAdded;
-  }
-
-  public boolean removeReview(Review aReview)
-  {
-    boolean wasRemoved = false;
-    if (reviews.contains(aReview))
-    {
-      reviews.remove(aReview);
-      wasRemoved = true;
+    // Hibernate requires a no-args constructor
+    protected Customer() {
+        super();
     }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addReviewAt(Review aReview, int index)
-  {  
-    boolean wasAdded = false;
-    if(addReview(aReview))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfReviews()) { index = numberOfReviews() - 1; }
-      reviews.remove(aReview);
-      reviews.add(index, aReview);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
 
-  public boolean addOrMoveReviewAt(Review aReview, int index)
-  {
-    boolean wasAdded = false;
-    if(reviews.contains(aReview))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfReviews()) { index = numberOfReviews() - 1; }
-      reviews.remove(aReview);
-      reviews.add(index, aReview);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addReviewAt(aReview, index);
+    public Customer(String password, String name, String email, PersonRole personRole, Wishlist wishlist) {
+        super(password, name, email, personRole);
+        this.wishlist = wishlist;
     }
-    return wasAdded;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfOrders()
-  {
-    return 0;
-  }
-  /* Code from template association_AddUnidirectionalMany */
-  public boolean addOrder(Order aOrder)
-  {
-    boolean wasAdded = false;
-    if (orders.contains(aOrder)) { return false; }
-    orders.add(aOrder);
-    wasAdded = true;
-    return wasAdded;
-  }
 
-  public boolean removeOrder(Order aOrder)
-  {
-    boolean wasRemoved = false;
-    if (orders.contains(aOrder))
-    {
-      orders.remove(aOrder);
-      wasRemoved = true;
-    }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addOrderAt(Order aOrder, int index)
-  {  
-    boolean wasAdded = false;
-    if(addOrder(aOrder))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfOrders()) { index = numberOfOrders() - 1; }
-      orders.remove(aOrder);
-      orders.add(index, aOrder);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
+    //------------------------
+    // INTERFACE
+    //------------------------
 
-  public boolean addOrMoveOrderAt(Order aOrder, int index)
-  {
-    boolean wasAdded = false;
-    if(orders.contains(aOrder))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfOrders()) { index = numberOfOrders() - 1; }
-      orders.remove(aOrder);
-      orders.add(index, aOrder);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addOrderAt(aOrder, index);
-    }
-    return wasAdded;
-  }
+    // Getters and Setters
 
-  public void delete()
-  {
-    Wishlist existingWishlist = wishlist;
-    wishlist = null;
-    if (existingWishlist != null)
-    {
-      existingWishlist.delete();
+    public Wishlist getWishlist() {
+        return wishlist;
     }
-    reviews.clear();
-    orders.clear();
-    super.delete();
-  }
 
+    public void setWishlist(Wishlist wishlist) {
+        this.wishlist = wishlist;
+    }
+
+    public List<Review> getReviews() {
+        return Collections.unmodifiableList(reviews);
+    }
+
+    public boolean addReview(Review review) {
+        return reviews.add(review);
+    }
+
+    public boolean removeReview(Review review) {
+        return reviews.remove(review);
+    }
+
+    public List<Order> getOrders() {
+        return Collections.unmodifiableList(orders);
+    }
+
+    public boolean addOrder(Order order) {
+        return orders.add(order);
+    }
+
+    public boolean removeOrder(Order order) {
+        return orders.remove(order);
+    }
+
+    @Override
+    public void delete() {
+        if (wishlist != null) {
+            wishlist.delete();
+        }
+        reviews.clear();
+        orders.clear();
+        super.delete();
+    }
 }
+
+// {
+
+//   //------------------------
+//   // ENUMERATIONS
+//   //------------------------
+
+//   public enum OrderStatus { ShoppingCart, Bought }
+
+//   //------------------------
+//   // MEMBER VARIABLES
+//   //------------------------
+
+//   //Customer Associations
+//   private Wishlist wishlist;
+//   private List<Review> reviews;
+//   private List<Order> orders;
+
+//   //------------------------
+//   // CONSTRUCTOR
+//   //------------------------
+
+//   public Customer(int aId, Wishlist aWishlist)
+//   {
+//     super(aId);
+//     if (aWishlist == null || aWishlist.getOwns() != null)
+//     {
+//       throw new RuntimeException("Unable to create Customer due to aWishlist. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+//     }
+//     wishlist = aWishlist;
+//     reviews = new ArrayList<Review>();
+//     orders = new ArrayList<Order>();
+//   }
+
+//   public Customer(int aId, int aWishlistIdForWishlist, Customer aCustomerForWishlist)
+//   {
+//     super(aId);
+//     wishlist = new Wishlist(aWishlistIdForWishlist, aCustomerForWishlist, this);
+//     reviews = new ArrayList<Review>();
+//     orders = new ArrayList<Order>();
+//   }
+
+//   //------------------------
+//   // INTERFACE
+//   //------------------------
+//   /* Code from template association_GetOne */
+//   public Wishlist getWishlist()
+//   {
+//     return wishlist;
+//   }
+//   /* Code from template association_GetMany */
+//   public Review getReview(int index)
+//   {
+//     Review aReview = reviews.get(index);
+//     return aReview;
+//   }
+
+//   public List<Review> getReviews()
+//   {
+//     List<Review> newReviews = Collections.unmodifiableList(reviews);
+//     return newReviews;
+//   }
+
+//   public int numberOfReviews()
+//   {
+//     int number = reviews.size();
+//     return number;
+//   }
+
+//   public boolean hasReviews()
+//   {
+//     boolean has = reviews.size() > 0;
+//     return has;
+//   }
+
+//   public int indexOfReview(Review aReview)
+//   {
+//     int index = reviews.indexOf(aReview);
+//     return index;
+//   }
+//   /* Code from template association_GetMany */
+//   public Order getOrder(int index)
+//   {
+//     Order aOrder = orders.get(index);
+//     return aOrder;
+//   }
+
+//   public List<Order> getOrders()
+//   {
+//     List<Order> newOrders = Collections.unmodifiableList(orders);
+//     return newOrders;
+//   }
+
+//   public int numberOfOrders()
+//   {
+//     int number = orders.size();
+//     return number;
+//   }
+
+//   public boolean hasOrders()
+//   {
+//     boolean has = orders.size() > 0;
+//     return has;
+//   }
+
+//   public int indexOfOrder(Order aOrder)
+//   {
+//     int index = orders.indexOf(aOrder);
+//     return index;
+//   }
+//   /* Code from template association_MinimumNumberOfMethod */
+//   public static int minimumNumberOfReviews()
+//   {
+//     return 0;
+//   }
+//   /* Code from template association_AddUnidirectionalMany */
+//   public boolean addReview(Review aReview)
+//   {
+//     boolean wasAdded = false;
+//     if (reviews.contains(aReview)) { return false; }
+//     reviews.add(aReview);
+//     wasAdded = true;
+//     return wasAdded;
+//   }
+
+//   public boolean removeReview(Review aReview)
+//   {
+//     boolean wasRemoved = false;
+//     if (reviews.contains(aReview))
+//     {
+//       reviews.remove(aReview);
+//       wasRemoved = true;
+//     }
+//     return wasRemoved;
+//   }
+//   /* Code from template association_AddIndexControlFunctions */
+//   public boolean addReviewAt(Review aReview, int index)
+//   {  
+//     boolean wasAdded = false;
+//     if(addReview(aReview))
+//     {
+//       if(index < 0 ) { index = 0; }
+//       if(index > numberOfReviews()) { index = numberOfReviews() - 1; }
+//       reviews.remove(aReview);
+//       reviews.add(index, aReview);
+//       wasAdded = true;
+//     }
+//     return wasAdded;
+//   }
+
+//   public boolean addOrMoveReviewAt(Review aReview, int index)
+//   {
+//     boolean wasAdded = false;
+//     if(reviews.contains(aReview))
+//     {
+//       if(index < 0 ) { index = 0; }
+//       if(index > numberOfReviews()) { index = numberOfReviews() - 1; }
+//       reviews.remove(aReview);
+//       reviews.add(index, aReview);
+//       wasAdded = true;
+//     } 
+//     else 
+//     {
+//       wasAdded = addReviewAt(aReview, index);
+//     }
+//     return wasAdded;
+//   }
+//   /* Code from template association_MinimumNumberOfMethod */
+//   public static int minimumNumberOfOrders()
+//   {
+//     return 0;
+//   }
+//   /* Code from template association_AddUnidirectionalMany */
+//   public boolean addOrder(Order aOrder)
+//   {
+//     boolean wasAdded = false;
+//     if (orders.contains(aOrder)) { return false; }
+//     orders.add(aOrder);
+//     wasAdded = true;
+//     return wasAdded;
+//   }
+
+//   public boolean removeOrder(Order aOrder)
+//   {
+//     boolean wasRemoved = false;
+//     if (orders.contains(aOrder))
+//     {
+//       orders.remove(aOrder);
+//       wasRemoved = true;
+//     }
+//     return wasRemoved;
+//   }
+//   /* Code from template association_AddIndexControlFunctions */
+//   public boolean addOrderAt(Order aOrder, int index)
+//   {  
+//     boolean wasAdded = false;
+//     if(addOrder(aOrder))
+//     {
+//       if(index < 0 ) { index = 0; }
+//       if(index > numberOfOrders()) { index = numberOfOrders() - 1; }
+//       orders.remove(aOrder);
+//       orders.add(index, aOrder);
+//       wasAdded = true;
+//     }
+//     return wasAdded;
+//   }
+
+//   public boolean addOrMoveOrderAt(Order aOrder, int index)
+//   {
+//     boolean wasAdded = false;
+//     if(orders.contains(aOrder))
+//     {
+//       if(index < 0 ) { index = 0; }
+//       if(index > numberOfOrders()) { index = numberOfOrders() - 1; }
+//       orders.remove(aOrder);
+//       orders.add(index, aOrder);
+//       wasAdded = true;
+//     } 
+//     else 
+//     {
+//       wasAdded = addOrderAt(aOrder, index);
+//     }
+//     return wasAdded;
+//   }
+
+//   public void delete()
+//   {
+//     Wishlist existingWishlist = wishlist;
+//     wishlist = null;
+//     if (existingWishlist != null)
+//     {
+//       existingWishlist.delete();
+//     }
+//     reviews.clear();
+//     orders.clear();
+//     super.delete();
+//   }
+
+// }
--- GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/model/PersonRole.java
+++ GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/model/PersonRole.java
@@ -1,53 +1,95 @@
 package ca.mcgill.ecse321.gamemanager.model;/*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.34.0.7242.6b8819789 modeling language!*/
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
 
+public abstract class PersonRole {
 
-// line 11 "model.ump"
-// line 114 "model.ump"
-public abstract class PersonRole
-{
+    //------------------------
+    // MEMBER VARIABLES
+    //------------------------
 
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
+    @Id
+    @GeneratedValue
+    private int id;
 
-  //PersonRole Attributes
-  private int id;
+    //------------------------
+    // CONSTRUCTOR
+    //------------------------
 
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
+    // JPA requires a no-argument constructor
+    protected PersonRole() {
+    }
 
-  public PersonRole(int aId)
-  {
-    id = aId;
-  }
+    // Constructor for setting the ID manually
+    public PersonRole(int aId) {
+        this.id = aId;
+    }
 
-  //------------------------
-  // INTERFACE
-  //------------------------
+    //------------------------
+    // GETTERS AND SETTERS
+    //------------------------
 
-  public boolean setId(int aId)
-  {
-    boolean wasSet = false;
-    id = aId;
-    wasSet = true;
-    return wasSet;
-  }
+    public int getId() {
+        return id;
+    }
 
-  public int getId()
-  {
-    return id;
-  }
+    public boolean setId(int aId) {
+        this.id = aId;
+        return true;
+    }
 
-  public void delete()
-  {}
+    //------------------------
+    // DELETE METHOD
+    //------------------------
 
-
-  public String toString()
-  {
-    return super.toString() + "["+
-            "id" + ":" + getId()+ "]";
-  }
+    public void delete() {
+    }
 }
+
+//   //------------------------
+//   // MEMBER VARIABLES
+//   //------------------------
+
+//   //PersonRole Attributes
+//   private int id;
+
+//   //------------------------
+//   // CONSTRUCTOR
+//   //------------------------
+
+//   public PersonRole(int aId)
+//   {
+//     id = aId;
+//   }
+
+//   //------------------------
+//   // INTERFACE
+//   //------------------------
+
+//   public boolean setId(int aId)
+//   {
+//     boolean wasSet = false;
+//     id = aId;
+//     wasSet = true;
+//     return wasSet;
+//   }
+
+//   public int getId()
+//   {
+//     return id;
+//   }
+
+//   public void delete()
+//   {}
+
+
+//   public String toString()
+//   {
+//     return super.toString() + "["+
+//             "id" + ":" + getId()+ "]";
+//   }
+// }
