35a753ed7b223823911342101ee3e9f2db4cbb5f
Ang <104416887+Neilllllt@users.noreply.github.com>
2024-10-11 14:19:17 -0400

    fixes

--- GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/repository/GameRepository.java
+++ GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/repository/GameRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.gamemanager.model.Game;
 
 public interface GameRepository extends CrudRepository<Game, Integer> {
-    public Game findGameById(int id);
+    public Game findGameByGameId(int id);
 }
 
 
