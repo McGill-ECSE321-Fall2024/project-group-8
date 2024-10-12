735b7bd1e3041c4fa4a36c43655c731a981bef42
dianeteo <teo.minxuan.diane@gmail.com>
2024-10-11 22:10:35 -0400

    working repositories (removed PersonRepository.java and edited find functions to appropriate format)

---
+++ GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/repository/GameRepository.java
@@ -0,0 +1,8 @@
+package ca.mcgill.ecse321.gamemanager.repository;
+
+import ca.mcgill.ecse321.gamemanager.model.Game;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GameRepository extends CrudRepository<Game, Integer> {
+    public Game findByGameId(int id);
+}
