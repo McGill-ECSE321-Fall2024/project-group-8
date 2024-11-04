08d57f8f57c2450a5beb03b5ca739df2e5471ee1
NaraYun0614 <nara.yun@mail.mcgill.ca>
2024-11-03 23:15:57 -0500

    Update CategoryContorller

--- GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/controller/CategoryController.java
+++ GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/controller/CategoryController.java
@@ -1,4 +1,68 @@
 package ca.mcgill.ecse321.gamemanager.controller;
 
+import ca.mcgill.ecse321.gamemanager.model.Category;
+import ca.mcgill.ecse321.gamemanager.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    // Get all categories
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return StreamSupport.stream(categoryRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+    // Get category by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Integer id) {
+        Optional<Category> category = categoryRepository.findById(String.valueOf(id));
+        return category.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    // Create a new category
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category savedCategory = categoryRepository.save(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    }
+
+    // Update category by ID
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Integer id, @RequestBody Category categoryDetails) {
+        Optional<Category> categoryData = categoryRepository.findById(String.valueOf(id));
+        if (categoryData.isPresent()) {
+            Category category = categoryData.get();
+            category.setName(categoryDetails.getName());
+            category.setDescription(categoryDetails.getDescription());
+            return ResponseEntity.ok(categoryRepository.save(category));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    // Delete category by ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Integer id) {
+        if (categoryRepository.existsById(String.valueOf(id))) {
+            categoryRepository.deleteById(String.valueOf(id));
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 }
