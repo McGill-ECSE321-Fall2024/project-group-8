791164301123f8528a475d80fc86d110a58285ef
plain-noodle-expert <yixuan.tang@mail.mcgill.ca>
2024-11-05 00:34:36 -0500

    Revert "Merge branch 'main' of https://github.com/McGill-ECSE321-Fall2024/project-group-8"
    This reverts commit 76525301c2c2964a052a5d18bc9a5b52a015916b, reversing
    changes made to 9f74b76b8ea06c61dc0e2d7473a8722ceec090bf.

--- GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/dto/PurchaseOrderDto.java
+++ GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/dto/PurchaseOrderDto.java
@@ -1,42 +1,4 @@
 package ca.mcgill.ecse321.gamemanager.dto;
 
-import ca.mcgill.ecse321.gamemanager.model.PurchaseOrder;
-import ca.mcgill.ecse321.gamemanager.model.PurchaseOrder.OrderStatus;
-
-import java.util.Date;
-
 public class PurchaseOrderDto {
-    private int orderId;
-    private OrderStatus orderStatus;
-    private Date date;
-
-    @SuppressWarnings("unused")
-    private PurchaseOrderDto() {}
-
-    public PurchaseOrderDto(PurchaseOrder model) {
-        this.date = model.getDate();
-        this.orderId = model.getOrderId();
-        this.orderStatus = model.getOrderStatus();
-    }
-
-    // Getters and setters
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-
-    public OrderStatus getOrderStatus() {return orderStatus; }
-
-    public void setOrderStatus(OrderStatus status) { this.orderStatus = status; }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
 }
