ddc2e41488af412e1c3fe3fd55fc742f3e95fb96
NaraYun0614 <nara.yun@mail.mcgill.ca>
2024-11-08 22:41:41 -0500

    Update

--- GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/controller/CategoryController.java
+++ GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/controller/CategoryController.java
@@ -1,68 +1,63 @@
 package ca.mcgill.ecse321.gamemanager.controller;
 
+import ca.mcgill.ecse321.gamemanager.dto.CategoryDto;
 import ca.mcgill.ecse321.gamemanager.model.Category;
-import ca.mcgill.ecse321.gamemanager.repository.CategoryRepository;
+import ca.mcgill.ecse321.gamemanager.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @RestController
-@RequestMapping("/api/categories")
+@RequestMapping("/categories")
 public class CategoryController {
 
     @Autowired
-    private CategoryRepository categoryRepository;
+    private CategoryService categoryService;
 
-    // Get all categories
     @GetMapping
-    public List<Category> getAllCategories() {
-        return StreamSupport.stream(categoryRepository.findAll().spliterator(), false)
+    public List<CategoryDto> getAllCategories() {
+        return categoryService.getAllCategories().stream()
+                .map(this::convertToDto)
                 .collect(Collectors.toList());
     }
 
-    // Get category by ID
     @GetMapping("/{id}")
-    public ResponseEntity<Category> getCategoryById(@PathVariable Integer id) {
-        Optional<Category> category = categoryRepository.findById(String.valueOf(id));
-        return category.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    public CategoryDto getCategoryById(@PathVariable int id) {
+        Category category = categoryService.getCategoryById(id);
+        return convertToDto(category);
     }
 
-    // Create a new category
     @PostMapping
-    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
-        Category savedCategory = categoryRepository.save(category);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryDto createCategory(@RequestBody CategoryDto categoryDto) {
+        Category category = new Category();
+        category.setName(categoryDto.getName());
+        category.setDescription(categoryDto.getDescription());
+        Category createdCategory = categoryService.createCategory(category);
+        return convertToDto(createdCategory);
     }
 
-    // Update category by ID
     @PutMapping("/{id}")
-    public ResponseEntity<Category> updateCategory(@PathVariable Integer id, @RequestBody Category categoryDetails) {
-        Optional<Category> categoryData = categoryRepository.findById(String.valueOf(id));
-        if (categoryData.isPresent()) {
-            Category category = categoryData.get();
-            category.setName(categoryDetails.getName());
-            category.setDescription(categoryDetails.getDescription());
-            return ResponseEntity.ok(categoryRepository.save(category));
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+    public CategoryDto updateCategory(@PathVariable int id, @RequestBody CategoryDto categoryDto) {
+        Category category = new Category();
+        category.setName(categoryDto.getName());
+        category.setDescription(categoryDto.getDescription());
+        Category updatedCategory = categoryService.updateCategory(id, category);
+        return convertToDto(updatedCategory);
     }
 
-    // Delete category by ID
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCategory(@PathVariable Integer id) {
-        if (categoryRepository.existsById(String.valueOf(id))) {
-            categoryRepository.deleteById(String.valueOf(id));
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable int id) {
+        categoryService.deleteCategory(id);
+    }
+
+    private CategoryDto convertToDto(Category category) {
+        return new CategoryDto(category.getCategoryId(), category.getName(), category.getDescription(), category.getGames().stream()
+                .map(game -> game.getGameId())
+                .collect(Collectors.toList()));
     }
 }
