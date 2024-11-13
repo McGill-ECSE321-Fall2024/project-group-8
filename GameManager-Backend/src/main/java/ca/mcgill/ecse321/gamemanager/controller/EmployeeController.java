af5df6b433fae96c4e86b1337839fda2fd821997
Sijing Gu <159672222+MRJ-G@users.noreply.github.com>
2024-11-13 01:08:39 -0500

    Finish unit testing for employee

--- GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/controller/EmployeeController.java
+++ GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/controller/EmployeeController.java
@@ -1,15 +1,15 @@
 package ca.mcgill.ecse321.gamemanager.controller;
 
+import ca.mcgill.ecse321.gamemanager.dto.EmployeeResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import ca.mcgill.ecse321.gamemanager.dto.EmployeeDto;
 import ca.mcgill.ecse321.gamemanager.dto.EmployeeRequestDto;
 import ca.mcgill.ecse321.gamemanager.model.Employee;
 import ca.mcgill.ecse321.gamemanager.service.EmployeeService;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -17,36 +17,40 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    // TODO: add new controller methods
+
     @GetMapping("/{email}")
-    public EmployeeDto findEmployeeByEmail(@PathVariable String email) {
+    public EmployeeResponseDto findEmployeeByEmail(@PathVariable String email) {
         Employee employee = employeeService.findEmployeeByEmail(email);
-        return convertToDto(employee);
+        return new EmployeeResponseDto(employee);
     }
 
     @GetMapping
-    public List<EmployeeDto> getAllEmployees() {
-        return employeeService.getAllEmployees().stream()
-                .map(this::convertToDto)
-                .collect(Collectors.toList());
+    public List<EmployeeResponseDto> getAllEmployees() {
+        List<Employee> employees = employeeService.getAllEmployees();
+        List<EmployeeResponseDto> employeesDtos = new ArrayList<>();
+        for (Employee employee : employees) {
+            employeesDtos.add(new EmployeeResponseDto(employee));
+        }
+        return employeesDtos;
     }
 
     @PostMapping
-    public EmployeeDto createEmployee(@RequestBody EmployeeRequestDto employeeRequestDto) {
+    public EmployeeResponseDto createEmployee(@RequestBody EmployeeRequestDto employeeRequestDto) {
         Employee createdEmployee = employeeService.createEmployee(
                 employeeRequestDto.getName(),
                 employeeRequestDto.getEmail(),
                 employeeRequestDto.getPassword());
-        return convertToDto(createdEmployee);
+        return new EmployeeResponseDto(createdEmployee);
     }
 
     @PutMapping("/{email}")
-    public EmployeeDto updateEmployee(@PathVariable String email, @RequestBody EmployeeRequestDto employeeRequestDto) {
+    public EmployeeResponseDto updateEmployee(@PathVariable String email, @RequestBody EmployeeRequestDto employeeRequestDto) {
         Employee updatedEmployee = employeeService.updateEmployee(
                 email,
                 employeeRequestDto.getName(),
-                employeeRequestDto.getEmail(),
                 employeeRequestDto.getPassword());
-        return convertToDto(updatedEmployee);
+        return new EmployeeResponseDto(updatedEmployee);
     }
 
     @DeleteMapping("/{email}")
@@ -54,7 +58,5 @@ public class EmployeeController {
         employeeService.deleteEmployee(email);
     }
 
-    private EmployeeDto convertToDto(Employee employee) {
-        return new EmployeeDto(employee.getName(), employee.getEmail());
-    }
+
 }
