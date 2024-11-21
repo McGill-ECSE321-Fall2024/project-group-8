6514bc1e5400fe9208bd8284061b14b33c9e1db3
NaraYun0614 <nara.yun@mail.mcgill.ca>
2024-11-21 15:18:42 -0500

    Update GameController

--- GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/controller/GameController.java
+++ GameManager-Backend/src/main/java/ca/mcgill/ecse321/gamemanager/controller/GameController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/games")
 public class GameController {
 
